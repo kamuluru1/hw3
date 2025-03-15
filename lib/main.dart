7e235746d3617e60786ccc254f6696149364fe32
Krishna Amuluru <krishna.amuluru@gmail.com>
2025-03-14 22:00:16 -0400

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,118 +6,230 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Card Matching Game',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const CardMatchingGame(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MemoryCard {
+  final int id;
+  bool isFaceUp;
+  bool isMatched;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MemoryCard({
+    required this.id,
+    this.isFaceUp = false,
+    this.isMatched = false,
+  });
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CardMatchingGame extends StatefulWidget {
+  const CardMatchingGame({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  State<CardMatchingGame> createState() => _CardMatchingGameState();
+}
+
+class _CardMatchingGameState extends State<CardMatchingGame> {
+  final int _numberOfPairs = 6;
+
+  late List<MemoryCard> _cards;
+  int _score = 0;
+  int _timeElapsed = 0;
+  Timer? _timer;
+
+  int? _previousFlippedIndex;
+
+  bool _isGameOver = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _startNewGame();
+  }
+
+  void _startNewGame() {
+    _cards = _generateShuffledCards(_numberOfPairs);
+    _score = 0;
+    _timeElapsed = 0;
+    _isGameOver = false;
+    _previousFlippedIndex = null;
+
+    _timer?.cancel();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _timeElapsed++;
+      });
     });
+  }
+
+  List<MemoryCard> _generateShuffledCards(int numberOfPairs) {
+    List<MemoryCard> cards = [];
+    for (int i = 0; i < numberOfPairs; i++) {
+      cards.add(MemoryCard(id: i));
+      cards.add(MemoryCard(id: i));
+    }
+    cards.shuffle();
+    return cards;
+  }
+
+  void _onCardTapped(int index) {
+    if (_cards[index].isFaceUp || _cards[index].isMatched || _isGameOver) {
+      return;
+    }
+
+    setState(() {
+      _cards[index].isFaceUp = true;
+
+      if (_previousFlippedIndex == null) {
+        _previousFlippedIndex = index;
+      } else {
+        final int previousIndex = _previousFlippedIndex!;
+        if (_cards[previousIndex].id == _cards[index].id) {
+          // It's a match
+          _cards[previousIndex].isMatched = true;
+          _cards[index].isMatched = true;
+          _score += 10;
+          _previousFlippedIndex = null;
+
+          if (_checkAllMatched()) {
+            _onGameOver();
+          }
+        } else {
+          _score -= 2;
+          Future.delayed(const Duration(seconds: 1), () {
+            setState(() {
+              _cards[previousIndex].isFaceUp = false;
+              _cards[index].isFaceUp = false;
+            });
+          });
+          _previousFlippedIndex = null;
+        }
+      }
+    });
+  }
+
+  bool _checkAllMatched() {
+    for (var card in _cards) {
+      if (!card.isMatched) return false;
+    }
+    return true;
+  }
+
+  void _onGameOver() {
+    setState(() {
+      _isGameOver = true;
+    });
+    _timer?.cancel();
+    _showVictoryDialog();
+  }
+
+  void _showVictoryDialog() {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Victory!'),
+          content: Text(
+            'You matched all pairs in $_timeElapsed seconds\n'
+                'Your score: $_score',
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Play Again'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _startNewGame();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Card Matching Game'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('Time: $_timeElapsed s',
+                    style: const TextStyle(fontSize: 18)),
+                Text('Score: $_score', style: const TextStyle(fontSize: 18)),
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              itemCount: _cards.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+              ),
+              padding: const EdgeInsets.all(8),
+              itemBuilder: (context, index) {
+                final card = _cards[index];
+                return GestureDetector(
+                  onTap: () => _onCardTapped(index),
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    decoration: BoxDecoration(
+                      color: card.isMatched
+                          ? Colors.green
+                          : (card.isFaceUp
+                          ? Colors.orange
+                          : Colors.blueAccent),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: card.isFaceUp || card.isMatched
+                          ? Text(
+                        'Card ${card.id}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      )
+                          : const Text(
+                        '?',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
